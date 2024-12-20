32368d15633699fbf2150af0bd68af1a379cf891
GASPing4ER <simecgasper@gmail.com>
2024-12-20 11:20:05 +0100

    new landing page video

--- app/components/BackgroundVideo.tsx
+++ app/components/BackgroundVideo.tsx
@@ -1,12 +1,14 @@
+// eslint-disable-next-line eslint-comments/disable-enable-pair
+/* eslint-disable jsx-a11y/media-has-caption */
+
 const BackgroundVideo = () => {
   return (
     <main className="h-screen relative">
       <video
-        src="/videos/hero.mp4"
+        src="/videos/about.mp4"
         className="object-cover w-full h-full hidden md:block"
         autoPlay
         loop
-        muted
       />
       <img
         src="/images/hero.webp"
