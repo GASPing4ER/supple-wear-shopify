dc095ad1137956011f41011ec790f8886a63fcc9
GASPing4ER <simecgasper@gmail.com>
2024-12-20 17:37:20 +0100

    deleted about page section

--- app/components/AfterFooter.tsx
+++ app/components/AfterFooter.tsx
@@ -1,6 +1,6 @@
 export const AfterFooter = () => {
   return (
-    <div className="flex justify-between py-6 px-6 md:px-8 lg:px-12">
+    <div className="flex flex-col md:flex-row items-center gap-4 justify-between py-6 px-6 md:px-8 lg:px-12">
       <p>© 2024 Supple. All rights reserved.</p>
       <a href="https://gsdigitalanchor.com">Built by G.S. Digital Anchor</a>
     </div>
--- app/components/BackgroundVideo.tsx
+++ app/components/BackgroundVideo.tsx
@@ -3,11 +3,16 @@ const BackgroundVideo = () => {
     <main className="h-screen relative">
       <video
         src="/videos/hero.mp4"
-        className="object-cover w-full h-full"
+        className="object-cover w-full h-full block"
         autoPlay
         loop
         muted
       />
+      {/* <img
+        src="/images/hero.webp"
+        alt="hero"
+        className="h-full w-full object-cover md:hidden"
+      /> */}
     </main>
   );
 };
