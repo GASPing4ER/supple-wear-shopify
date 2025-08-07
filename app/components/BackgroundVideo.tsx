0dc7ab92e931f9b226537ef6aed0536f2eb587cc
GASPing4ER <simecgasper@gmail.com>
2025-08-07 13:13:51 +0200

    essentia collection video and heading

--- app/components/BackgroundVideo.tsx
+++ app/components/BackgroundVideo.tsx
@@ -2,21 +2,24 @@ const BackgroundVideo = () => {
   return (
     <main className="h-[calc(100vh-60px)] relative">
       <video
-        src="/videos/hero.mp4"
+        src="/videos/essentia.mp4"
         className="object-cover w-full h-full block"
         autoPlay
         loop
         muted
         playsInline
       />
+      <h1 className="philosopher absolute text-white z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl font-bold text-center">
+        ESSENTIA - AUGUST LINE
+      </h1>
       {/* <img
         src="/images/hero.webp"
         alt="hero"
         className="h-full w-full object-cover md:hidden"
       /> */}
-      <h1 className="philosopher absolute text-white z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl lg:hidden font-bold text-center">
+      {/* <h1 className="philosopher absolute text-white z-100 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-4xl lg:hidden font-bold text-center">
         Welcome to Supple
-      </h1>
+      </h1> */}
     </main>
   );
 };
