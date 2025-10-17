134933520f4ed7c8e1636989c6a942ee8d5d41c1
GASPing4ER <simecgasper@gmail.com>
2025-10-17 15:51:42 +0200

    added new videos

--- app/routes/($locale)._index.tsx
+++ app/routes/($locale)._index.tsx
@@ -114,6 +114,13 @@ export default function Homepage() {
   return (
     <>
       <BackgroundVideo />
+      <div className="flex justify-center pt-10">
+        <img
+          src="/images/supple-drop2.jpeg"
+          alt="Supple Drop II"
+          className="w-[400px] object-cover h-full"
+        />
+      </div>
       <Section className="text-center uppercase text-lg lg:text-2xl philosopher">
         <h2>Let’s plié, roll up, squat, child pose, and brunch together.</h2>
       </Section>
